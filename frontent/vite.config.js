6ebcebc9abc95be570a52d71903364e33601b290
aditya01sah <aditya2002shah@gmail.com>
2026-03-15 15:11:42 +0530

    Configure deployment settings for GitHub Pages (Frontend) and Render (Backend)

--- frontent/vite.config.js
+++ frontent/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Job-Hunt/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
